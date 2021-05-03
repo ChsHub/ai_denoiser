99f040103c6675cc4a1581015db19dcf762913b6
ChsHub <christian1193@web.de>
2021-05-03 18:49:09 +0200

    Add docu;

--- src/image_dataset.py
+++ src/image_dataset.py
@@ -1,25 +1,32 @@
 from logging import info
-from logging import info
 from os import walk
 from os.path import join
 
-import torch
 from PIL import Image
 from numpy import asarray, add, uint8, clip
 from numpy.random import randint
+from torch import Tensor
 from torch.utils.data import Dataset
 
 from transforms.to_tensor import ToTensor
 
 
-def show_image(input_tensor, output_tensor, index):
-    Image.fromarray(input_tensor.numpy()[index][0][:, :], 'L').show()
-    Image.fromarray(output_tensor.numpy()[index, :, :, ::], 'RGB').show()
+def show_image(input_tensor: Tensor, index: int) -> None:
+    """
+    Display image from Tensor
+    :param input_tensor: Tensor to show
+    :param index: Image number in tensor
+    """
+    Image.fromarray(input_tensor.numpy()[index, :, :, ::], 'RGB').show()
 
 
-def _noise_open(path):
+def _noise_open(path: str) -> Image:
+    """
+    Open image and apply random noise
+    :param path: Image path
+    :return: Image object
+    """
     result = Image.open(path)
-    size = result.size
     result = asarray(result)
     noise = randint(-10, 10, result.shape)
     result = add(result, noise)
@@ -30,6 +37,7 @@ def _noise_open(path):
 
 
 class ImageDataset(Dataset):
+
     # https://pytorch.org/tutorials/recipes/recipes/custom_dataset_transforms_loader.html?highlight=dataloader
     def __init__(self, image_directory: str = '../resources/dataset', size: int = 20, transform=None):
         """
@@ -47,7 +55,14 @@ class ImageDataset(Dataset):
         self._image_generator = self.generate_slice()
         self._noise_image_generator = self.generate_slice(_noise_open)
 
-    def get_slice_count(self, image_directory, size):
+    @staticmethod
+    def get_slice_count(image_directory: str, size: int) -> int:
+        """
+        Count how many image slices can be generated from all dataset images
+        :param image_directory: Directory path containing images
+        :param size: Height and width of square image slices
+        :return: Total slice count
+        """
         counter = 0
         for root, _, files in walk(image_directory):
             for file in files:
@@ -56,13 +71,19 @@ class ImageDataset(Dataset):
                     counter += (size_x // size) * (size_y // size)
         return counter
 
-    def __len__(self):
+    def __len__(self) -> int:
         """
-        Size of the dataset
+        Get size of dataset
+        :return: Number of image slices
         """
         return self._num_slices
 
     def generate_slice(self, _image_open=Image.open):
+        """
+        Generator method for image slices
+        :param _image_open: Method to open images
+        :return:
+        """
         for root, _, files in walk(self._image_directory):
             for file in files:
                 with _image_open(join(root, file)) as image:
@@ -77,8 +98,8 @@ class ImageDataset(Dataset):
         Creating 3D Tensor from image. Array shape is nChannels x Height x Width.
         """
 
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
+        # if torch.is_tensor(idx):
+        #    idx = idx.tolist()
 
         sample = {'image': next(self._noise_image_generator),
                   'character': next(self._image_generator)}
