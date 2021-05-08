17b9d2660d2d01e3c69d4863e8d1ecaa48fa880d
ChsHub <christian1193@web.de>
2021-05-08 14:50:33 +0200

    image_denoiser.py: refactor, add loss to accuracy check, fix loss in net save;

--- image_denoiser.py
+++ image_denoiser.py
@@ -30,27 +30,60 @@ def get_cuda_device():
     return device
 
 
-def get_accuracy(net, testloader):
+def print_accuracy(net, test_loader) -> None:
+    """
+    Test accuracy of the network, using test data not used during training
+    :param net: Current network
+    :param test_loader: Dataload of test data
+    """
     correct = 0
     total = 0
+    criterion = nn.L1Loss(reduction='mean')
+    loss_total = 0
     with torch.no_grad():
-        for data in testloader:
+        for data in test_loader:
             images = data[0]
             labels = data[1]
             outputs = net(images)
             total += labels.size(0)
             correct += (outputs == labels).sum().item()
+            # Get loss
+            loss = criterion(outputs, labels)
+            loss_total += loss.item()
 
-    info('Accuracy of the network on the training set: %.4f %%' % (100 * correct / total))
+    info('Accuracy of the network on the training set: %.4f %% Mean loss: %.4f' % (100 * correct / total,
+                                                                                   loss_total / len(test_loader)))
 
 
-def get_info(string: float, length=12) -> str:
+def format_number(string: float, length=12) -> str:
+    """
+    Convert float to fixed length str
+    :param string: Float number
+    :param length: Length of output string
+    :return: String of number
+    """
     string = "%.10f" % string
     length -= len(string)
     return ' ' * length + string
 
 
-def save_state(net, running_loss: float, epoch: int, last_save: int, periodic_save_time: int = 1) -> int:
+def get_loss_info(epoch, running_loss, prev_loss, last_loss, counter):
+    """
+    Print trainings info
+    :param epoch: Current epoch
+    :param running_loss: Running loss of current epoch
+    :param prev_loss: Loss of previous epoch
+    :param last_loss: Loss of previously saved epoch
+    :param counter: Number of batches in one trainings epoch
+    :return: String containing current trainings info
+    """
+    return '[%d] loss: %s\t%s\t%s  ' % (epoch,
+                                        format_number(running_loss / counter),
+                                        format_number((prev_loss - running_loss) / counter),
+                                        format_number((last_loss - running_loss) / counter))
+
+
+def save_state(net, running_loss: float, epoch: int, last_save: int, periodic_save_time: int = 10) -> int:
     """
     Save state if specified time has past
     :param net: Net
@@ -66,19 +99,20 @@ def save_state(net, running_loss: float, epoch: int, last_save: int, periodic_sa
             error('Loss is nan [%s]' % epoch)
             raise ValueError()
 
-        torch.save(net.state_dict(), join(net_dir, '[%s] %.4f.pth' % (epoch, running_loss)))
+        file_name = '[%s] %.4f.pth' % (epoch, running_loss)
+        torch.save(net.state_dict(), join(net_dir, file_name))
         last_save = perf_counter_ns()
-        info('STATE SAVED')
+        info('STATE SAVED ' + file_name)
 
         # Delete previous states
         net_states = listdir(net_dir)
-        while len(net_states) > 30:
+        while len(net_states) > 10:
             send2trash(join(net_dir, net_states.pop(0)))  # delete old state
 
     return last_save
 
 
-def train_network(dataset_path, device, lr, momentum, batch_size, check_accuracy=False):
+def train_network(dataset_path, device, lr, momentum, batch_size: int, check_accuracy=False):
     """
     Trainings loop
     :param dataset_path: Directory for dataset images
@@ -103,23 +137,25 @@ def train_network(dataset_path, device, lr, momentum, batch_size, check_accuracy
     # Look at accuracy from trained net
     if check_accuracy:
         with Timer('Get accuracy', log_function=info):
-            get_accuracy(net, DataLoader(ImageDataset('resources/test_dataset', transform=ToTensor()),
-                                         batch_size=batch_size, shuffle=True, num_workers=0))
+            print_accuracy(net, DataLoader(ImageDataset('resources/test_dataset', transform=ToTensor()),
+                                           batch_size=batch_size, shuffle=True, num_workers=0))
 
     # Load Optimizer and Loss function
     criterion = nn.L1Loss(reduction='mean')
     optimizer = optim.SGD(net.parameters(), lr=lr, momentum=momentum)
     prev_loss = last_loss
     repetitions = 60
-    counter = repetitions * len(train_loader) // batch_size
+    # counter = repetitions * len(train_loader) // batch_size
     last_save = perf_counter_ns()
 
     # Loop over the dataset multiple times
     for epoch in range(last_epoch + 1, 48000):
         running_loss = 0.0
+        counter = 0
         with Timer('Net Training: Epoch', log_function=info) as timer:
             for _ in range(repetitions):
                 for data in train_loader:
+                    counter += 1
                     # Zero the parameter gradients
                     for param in net.parameters():
                         param.grad = None
@@ -132,12 +168,9 @@ def train_network(dataset_path, device, lr, momentum, batch_size, check_accuracy
                     loss.backward()
                     optimizer.step()
                     running_loss += loss.item()
-                    last_save = save_state(net, running_loss, epoch, last_save)
+                    last_save = save_state(net, running_loss / counter, epoch, last_save)
 
-            timer._message += '[%d] loss: %s\t%s\t%s  ' % (epoch,
-                                                           get_info(running_loss / counter),
-                                                           get_info((prev_loss - running_loss) / counter),
-                                                           get_info((last_loss - running_loss) / counter))
+            timer._message += get_loss_info(epoch, running_loss, prev_loss, last_loss, counter)
 
         prev_loss = running_loss
 
@@ -148,4 +181,4 @@ if __name__ == '__main__':
     with Logger(debug=True, max_logfile_count=50):
         torch.autograd.set_detect_anomaly(False)  # Turn off for performance
         device = get_cuda_device()
-        train_network(dataset_path, device, lr=0.001, momentum=0.4, batch_size=4)
+        train_network(dataset_path, device, lr=0.001, momentum=0.4, batch_size=4, check_accuracy=True)
