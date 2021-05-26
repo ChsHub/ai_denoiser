denoise_net.py: state load bug fix; image_dataset.py: implement denoise_image; test_to_tensor.py: test normalizing; to_tensor.py: fix normalization, implement unnormalization;

image_denoiser.py
@@ -9,7 +9,7 @@ from timerpy import Timer
 from torch import nn, optim
 from torch.utils.data import DataLoader
 
-from saver import Saver
+from src.saver import Saver
 from src.denoise_net import Net
 from src.image_dataset import ImageDataset
 from src.paths import dataset_path
@@ -135,8 +135,6 @@ def train_network(dataset_path, device, lr, momentum, batch_size: int, check_acc
                     saver.running_loss += loss.item()
 
             saver.timer._message += get_loss_info(saver.epoch, saver.running_loss, last_loss, saver.counter)
-
-    info('Finished Training')
 
 
 if __name__ == '__main__':

src/denoise_net.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from logging import info, error
-from os import listdir
-from os.path import join
+from os import listdir, remove
+from os.path import join, exists
 
 import torch
 from numpy import product
@@ -23,7 +23,7 @@ class Net(nn.Module):
         self.conv3 = nn.Conv2d(in_channels=convolutions[1], out_channels=convolutions[2], kernel_size=3)
         self.flat_features = 14 * 14 * convolutions[2]  # TODO SMALLER THAN IMAGE?
         self.fc1 = nn.Linear(self.flat_features, size * size * 3)
-        self.fc2 = nn.Linear(size * size * 3, size * size * 3)
+        # self.fc2 = nn.Linear(size * size * 3, size * size * 3)
         info('CONV LAYERS: %s' % convolutions)
         # info('NUM NEURONS: %s' % (convolutions[0] * 24 * 24 + convolutions[1] * 4 * 4))
 
@@ -34,7 +34,7 @@ class Net(nn.Module):
         x = self.conv3(x)
         x = x.view(-1, self.flat_features)
         x = functional.relu(self.fc1(x))
-        x = self.fc2(x)
+        # x = self.fc2(x)
         return x
 
     def num_flat_features(self, x):
@@ -47,20 +47,23 @@ class Net(nn.Module):
         :param directory: Net state directory
         :return: Last epoch number, and last loss value
         """
-        try:
-            net_states = listdir(directory)
-            if net_states:
-                last_state = net_states[-1]
-                self.load_state_dict(torch.load('nets/%s' % last_state))  # load last net
-                info('Load: %s' % last_state)
+        if not exists(directory):
+            info('path does not exists')
+            return 0, 0.0
 
-                _, last_loss, last_epoch = last_state.split(' ')
-                last_loss = float(last_loss)
-                last_epoch = int(last_epoch[1:-5])
+        net_states = listdir(directory)
+        if net_states:
+            last_state = net_states[-1]
+            self.load_state_dict(torch.load(join(directory, last_state)))  # load last net
+            info('Load: %s' % last_state)
 
-                return last_epoch, last_loss
-        except Exception:
-            info('Network changed')
+            _, last_loss, last_epoch = last_state.split(' ')
+            last_loss = float(last_loss)
+            last_epoch = int(last_epoch[1:-5])
+
+            return last_epoch, last_loss
+
+        info('Network changed')
         return 0, 0.0
 
     def save_state(self, running_loss: float, epoch: int, directory: str = 'nets') -> None:
@@ -81,4 +84,4 @@ class Net(nn.Module):
         # Delete previous states
         net_states = listdir(directory)
         while len(net_states) > 10:
-            send2trash(join(directory, net_states.pop(0)))  # Delete old state
+            remove(join(directory, net_states.pop(0)))  # Delete old state

src/image_dataset.py
@@ -3,13 +3,16 @@ from os import walk
 from os.path import join, exists, splitext
 from random import shuffle, randint
 
+import torch
 from PIL import Image
+from logger_default import Logger
 from numpy import asarray, add, uint8, clip, array
 from numpy.random import randint
+from torch import Tensor
 from torch.utils.data import Dataset
 
-from transforms.to_tensor import ToTensor
-from denoise_net import Net
+from src.denoise_net import Net
+from transforms.to_tensor import ToTensor, unnormalize, get_normalized_tensor
 
 
 def _noise_open(path: str) -> (array, int, int):
@@ -122,18 +125,57 @@ class ImageDataset(Dataset):
 
         return sample
 
+    def _denoise_tile(self, net, generator):
+        input = self.transform([next(generator)])
+        input = Tensor(input[0])
+        input = input.unsqueeze(0)
+        output = net(input)
+        return output.reshape((3, self._size, self._size))
+
     def denoise_image(self, path):
+        """
+        Denoise image by using the trained net
+        :param path: Image path
+        :return: None
+        """
         net = Net(self._size)
-        last_epoch, last_loss = net.load_last_state()
-        for input in self.generate_slice([path], self._size, (3 * self._size * self._size), _normal_open):
-            output = net(input)
+        last_epoch, last_loss = net.load_last_state('../nets')
+        image = []
+        with Image.open(path) as img:
+            width, height = img.size
+            width -= (width % self._size)
+            height -= (height % self._size)
+
+        for file in [path]:
+            data, size_x, size_y = _normal_open(file)
+            data = data.copy()
+            for x in range(0, size_x - self._size, self._size):
+                for y in range(0, size_y - self._size, self._size):
+                    tile1 = data[y:y + self._size, x:x + self._size, :3]  # .reshape((3, self._size, self._size))
+                    tile = get_normalized_tensor(tile1)
+                    tile = tile[0].unsqueeze(0)
+                    tile = net(tile)
+                    tile = tile.reshape((self._size, self._size, 3))
+                    tile = unnormalize(tile)
+
+                    for i in range(self._size):
+                        for j in range(self._size):
+                            for w in range(3):
+                                if int(tile[i, j, w]) != int(tile1[i, j, w]):
+                                    print(int(tile[i, j, w]), int(tile1[i, j, w]))
+                                    print(tile1)
+                    data[y:y + self._size, x:x + self._size, :3] = tile
+
+        Image.fromarray(data).show()
 
 
 if __name__ == '__main__':
-    dataset = ImageDataset('../resources/dataset', transform=ToTensor())
-    dataset.denoise_image("")
-    print(len(dataset))
-    item = dataset[0]
-    item = dataset[0]
-    # show_image(item[0])
-    print(item)
+    with Logger(debug=True):
+        dataset = ImageDataset('../resources/dataset', transform=ToTensor())
+        with torch.no_grad():
+            dataset.denoise_image("")
+        print(len(dataset))
+        item = dataset[0]
+        item = dataset[0]
+        # show_image(item[0])
+        print(item)

transforms/to_tensor.py
@@ -4,6 +4,7 @@ import numpy
 import numpy as np
 import torch
 from PIL import Image
+from numpy import ndarray, multiply, int16, divide, subtract
 from torch import tensor
 
 
@@ -12,16 +13,32 @@ def show_image(n: tensor):
     Undo normalization and show image
     :param n: Image tensor
     """
-    n = n.numpy()
-    n += 1  # Pixel range [0, 2]
-    n = np.multiply(n, 127.5)  # Pixel range [0, 255]
-    n = n.astype(numpy.uint8)
+    n = unnormalize(n)
     n = n.reshape((n.shape[1], n.shape[2], n.shape[0]))
     Image.fromarray(n).show()
 
 
-def get_normalized_tensor(input_tensor):
-    input_tensor = input_tensor * 2 / 255 - 1  # Convert image values from [0,255] to [-1,1]
+def unnormalize(n: tensor):
+    """
+    Turn Tensor into image tile
+    :param n: Tensor with value range [-1, 1]
+    :return: Numpy image tile with value range [0, 255]
+    """
+    n = n.numpy()
+    n += 1.0  # [-1,1] to [0,2]
+    n = multiply(n, 127.5001)  # [0,2] to [0, 255]
+    n = n.astype(numpy.uint8)
+    return n
+
+
+def get_normalized_tensor(input_tensor: ndarray):
+    """
+    Turn Image tile into Tensor
+    :param input_tensor: Numpy image tile with value range [0, 255]
+    :return: Tensor with value range [-1, 1]
+    """
+    input_tensor = divide(input_tensor, 127.5, dtype=np.float)  # Convert image values from [0,255] to [0,2]
+    input_tensor = subtract(input_tensor, 1.0)  # Convert image values from [0,2] to [-1,1]
     input_tensor = tensor(input_tensor.copy(), dtype=torch.float)
     return input_tensor
 